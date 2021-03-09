f9d035fc1d5be3fd889e9e2fa1ef84e6448c157d
Edin Ramírez <diamondz.06@live.com.mx>
2021-03-08 19:41:35 -0600

    Read_Api
    Se muestran los datos del api  en una lista

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-
-const Movie = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Studio Ghibli"),
-    React.createElement("h1", {}, "Movie"),
-  ]);
-};
-
+import MovieResults from "./MovieResults";
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Studios Ghibli"),
-    React.createElement(Movie),
-  ]);
+  return (
+    <div>
+      <MovieResults />
+    </div>
+  );
 };
 
 render(React.createElement(App), document.getElementById("root"));
